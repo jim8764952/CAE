dca1fd8065c02ed096688b3a3689c5d131164fd9
jim8764952 <jim8764952@gmail.com>
2024-04-08 01:02:55 +0800

    Initial commit

---
+++ src/test/java/com/example/cae/CaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

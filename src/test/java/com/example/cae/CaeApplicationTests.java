18b132bdbb637693fd277611858010e9ef84f90e
jim8764952 <jim8764952@gmail.com>
2024-04-08 15:12:19 +0800

    增加lombok來省去Setter Getter

--- src/test/java/com/example/cae/CaeApplicationTests.java
+++ src/test/java/com/example/cae/CaeApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CaeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
